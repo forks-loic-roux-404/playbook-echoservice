4554ab2329b7faa303f867a9102a240dbfd69c80
loic-roux <69loloro10@gmail.com>
2020-01-25 18:21:57 +0100

    arrange databse duplication system

--- roles/mysql/templates/cron.sh
+++ roles/mysql/templates/cron.sh
@@ -1,20 +1,18 @@
-#############
 # variables #
-date= $(date '+%F')
-script_file="{{cron_workspace}}/{{ projectname }}_$date.sql"
-remote_db_file="{{cron_workspace}}/{{ projectname }}_remote_$date.sql"
-####################################
+script_file="{{cron_workspace}}/{{ projectname }}_.sql"
+remote_db_file="{{cron_workspace}}/{{ projectname }}_remote_.sql"
+
+rm -rf $script_file || true
+rm -rf $remote_db_file || true
+
 # get remote db data as sql script #
-mysqldump -t --insert-ignore -u {{ backup_user }}  -p{{ backup_pw }}  -h {{ backup_host }} {{ backup_db }} > $remote_db_file
-###################################
+mysqldump -t --single-transaction --insert-ignore -u {{ backup_user }} -p{{ backup_pw }} -h {{ backup_host }} {{ backup_db }} > $remote_db_file
+
 # insert remote data in actual db #
-mysql -u {{ mysql_user }} -p{{ mysql_pw }} -h {{ mysql_bind_address }} {{ mysql_db }} < $remote_db_file &
-########################################################################
+mysql -u {{ mysql_user }} -p{{ mysql_pw }} -h {{ mysql_host }} {{ mysql_db }} < $remote_db_file &
 ##################### Reinsert data in remote db #######################
-########################################################################
 # dump date sorted sql scripts / this script will add all missing rows #
-mysqldump -t --insert-ignore --skip-opt -u {{ mysql_user }}  -p{{ mysql_pw }}  -h {{ mysql_bind_address }} {{ mysql_db }} > $script_file
-######################################################
-# Connect to remote database and execute dump script #
-mysql -u {{ backup_user }} -p{{ backup_pw }} -h {{ backup_host }} {{ backup_db }} < $script_file &
-######################################################
+mysqldump -t --insert-ignore --skip-opt -u {{ mysql_user }}  -p{{ mysql_pw }} -h {{ mysql_host }} {{ mysql_db }} > $script_file
+
+# Connect to remote database and execute dump script # NOT USED (danger)
+# mysql -u {{ backup_user }} -p{{ backup_pw }} -h {{ backup_host }} {{ backup_db }} < $script_file &
